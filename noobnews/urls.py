08d5c941e057869e171c16441cfeeaca88ac5c36
Misahael Fernandez <2372351f@student.gla.ac.uk>
2019-02-17 01:26:20 +0000

    Basic implementation of login and  uaser registration. - Missing validations

--- noobnews/urls.py
+++ noobnews/urls.py
@@ -21,8 +21,10 @@ from noobnews import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^suggest/$', views.suggest_category, name='suggest_category'),
-    url(r'^videogame/(?P<videogame_name_slug>[\w\-]+)/$', views.show_videogame, name='show_videogame'),
+    url(r'^videogame/(?P<videogame_name_slug>[\w\-]+)/$',
+        views.show_videogame, name='show_videogame'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^register/$', views.register, name='register'),
     url('', include('social_django.urls', namespace='social')),
 ]
